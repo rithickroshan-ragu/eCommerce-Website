70b8aaf77afef712b3979b72305e392941935fbd
Rithickroshan Ragu <rrithick116@gmail.com>
2025-11-08 15:03:23 +0530

    Added Models(ORM) for Products and category

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from products.models import Product
 
 def HelloViews(request):
     print(request.headers)
@@ -7,5 +8,10 @@ def HelloViews(request):
 
 
 def HelloWorld(request, username):
-    print(username)
+    p = Product.objects.all()
+    print(p)
+    print(p[1])
+    print(p[1].__dict__)
+    print(p[1].name)
+
     return JsonResponse({"Hello":username})
